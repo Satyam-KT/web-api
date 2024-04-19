37175c5a00cecbf4f6ab464ae86169fcb7380cba
Satyam-KT <kumarsatyamara1082@gmail.com>
2024-04-19 19:12:52 +0530

    generate otp

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 
 const app = express();
 const port = 3000;
@@ -16,12 +17,27 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 // Define a User schema
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    otp: { type: String}
 });
 const User = mongoose.model('User', userSchema);
 
 // Middleware
 app.use(bodyParser.json());
+
+// Nodemailer configuration
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'testnodemail1082@gmail.com',
+    pass: 'zikxzmhgsnxzwsxl'
+  }
+});
+
+// Generate a random 6-digit OTP
+function generateOTP(){
+  return Math.floor(100000 + Math.random() * 900000).toString();
+}
 
 // API endpoint for user registration
 app.post('/register', async (req, res) => {
@@ -31,9 +47,20 @@ app.post('/register', async (req, res) => {
       if (existingUser) {
         return res.status(400).json({ message: 'User already exists' });
       }
+      const otp = generateOTP();
       const newUser = new User({ email, password });
       await newUser.save();
-      res.status(201).json({ message: 'User registered successfully' });
+
+      // Send otp via email
+      const mailOptions = {
+        from: 'testnodemail1082@gmail.com',
+        to: email,
+        subject: 'OTP Verification',
+        text: `Your OTP for registration is: ${otp}`
+      }
+      await transporter.sendMail(mailOptions);
+
+      res.status(201).json({ message: 'User registered successfully. OTP sent to your email' });
     } catch (error) {
       res.status(500).json({ message: 'Internal server error' });
     }
